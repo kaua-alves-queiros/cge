9caf1c1604bd77d0fd482e6d45a8de154bcc2c59
Kauã Q. <89699012+kaua-alves-queiros@users.noreply.github.com>
2025-08-08 23:54:21 +0000

    add game object hearder

---
+++ include/game_object.h
@@ -0,0 +1,40 @@
+#ifndef CGE_GAMEOBJECT_H
+#define CGE_GAMEOBJECT_H
+
+typedef struct cge_game_object cge_game_object;
+typedef struct cge_game_object_list_node cge_game_object_list_node;
+
+struct cge_game_object
+{
+    char* name;
+    cge_game_object_list_node* children;
+    void(*on_start)(cge_game_object* self);
+    void(*on_update)(cge_game_object* self);
+    void(*on_end)(cge_game_object* self);
+    void* model;
+};
+
+struct cge_game_object_list_node
+{
+    cge_game_object* child;
+    cge_game_object_list_node* next;
+};
+
+cge_game_object* cge_game_object_create(
+    char* name,
+    cge_game_object_list_node* children,
+    void(*on_start)(cge_game_object* self),
+    void(*on_update)(cge_game_object* self),
+    void(*on_end)(cge_game_object* self),
+    void* model
+);
+void cge_game_object_add_child(cge_game_object* self, cge_game_object* child);
+cge_game_object* cge_get_child_by_name(cge_game_object* self, char* name);
+cge_game_object* cge_game_object_remove(cge_game_object* self, char* name);
+void cge_game_object_free(cge_game_object* self);
+
+void cge_game_object_call_on_start(cge_game_object* self);
+void cge_game_object_call_on_update(cge_game_object* self);
+void cge_game_object_call_on_end(cge_game_object* self);
+
+#endif
