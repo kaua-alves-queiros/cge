9d55cc78813aa0aed6f12722b0e2e8ea9ed03d08
Kauã Q. <89699012+kaua-alves-queiros@users.noreply.github.com>
2025-08-09 14:09:26 +0000

    start implement new design in vector3

--- engine/source/vector3.c
+++ engine/source/vector3.c
@@ -1,169 +1,192 @@
 #include "../include/vector3.h"
 #include <math.h>
 
-cge_vector3* cge_vector3_create(float x, float y, float z)
+void cge_vector3_add(float* a, float* b, float* out)
 {
-    cge_vector3* self = (cge_vector3*)malloc(sizeof(cge_vector3));
-    self->x = x;
-    self->y = y;
-    self->z = z;
-    return self;
+    out[0] = a[0] + b[0];
+    out[1] = a[1] + b[1];
+    out[2] = a[2] + b[2];
 }
 
-void cge_vector3_add(cge_vector3* self, cge_vector3* to_add)
+void cge_vector3_subtract(float* a, float* b, float* out)
 {
-    self->x += to_add->x;
-    self->y += to_add->y;
-    self->z += to_add->z;
+    out[0] = a[0] - b[0];
+    out[1] = a[1] - b[1];
+    out[2] = a[2] - b[2]; 
 }
 
-void cge_vector3_subtract(cge_vector3* self, cge_vector3* to_subtract)
+void cge_vector3_multiply(float* a, float* b, float* out)
 {
-    self->x -= to_subtract->x;
-    self->y -= to_subtract->y;
-    self->z -= to_subtract->z;
+    out[0] = a[0] * b[0];
+    out[1] = a[1] * b[1];
+    out[2] = a[2] * b[2];
 }
 
-void cge_vector3_multiply(cge_vector3* self, cge_vector3* to_multiply)
+void cge_vector3_divide(float* a, float* b, float* out)
 {
-    self->x = self->x * to_multiply->x;
-    self->y = self->y * to_multiply->y;
-    self->z = self->z * to_multiply->z;
+    out[0] = a[0] / b[0];
+    out[1] = a[1] / b[1];
+    out[2] = a[2] / b[2];
 }
 
-void cge_vector3_divide(cge_vector3* self, cge_vector3* to_divide)
+void cge_vector3_add_scalar(float* a, float b, float* out)
 {
-    self->x = self->x / to_divide->x;
-    self->y = self->y / to_divide->y;
-    self->z = self->z / to_divide->z;
+    out[0] = a[0] + b;
+    out[1] = a[1] + b;
+    out[2] = a[2] + b;
 }
 
-void cge_vector3_add_scalar(cge_vector3* self, float to_add)
+void cge_vector3_subtract_scalar(float* a, float b, float* out)
 {
-    self->x += to_add;
-    self->y += to_add;
-    self->z += to_add;
+    out[0] = a[0] - b;
+    out[1] = a[1] - b;
+    out[2] = a[2] - b;
 }
 
-void cge_vector3_subtract_scalar(cge_vector3* self, float to_subtract)
+void cge_vector3_multiply_scalar(float* a, float b, float* out)
 {
-    self->x -= to_subtract;
-    self->y -= to_subtract;
-    self->z -= to_subtract;
+    out[0] = a[0] * b;
+    out[1] = a[1] * b;
+    out[2] = a[2] * b;
 }
 
-void cge_vector3_multiply_scalar(cge_vector3* self, float to_multiply)
+void cge_vector3_divide_scalar(float* a, float b, float* out)
 {
-    self->x = self->x * to_multiply;
-    self->y = self->y * to_multiply;
-    self->z = self->z * to_multiply;
+    out[0] = a[0] / b;
+    out[1] = a[1] / b;
+    out[2] = a[2] / b;
 }
 
-void cge_vector3_divide_scalar(cge_vector3* self, float to_divide)
+void cge_vector3_negate(float* a, float* out)
 {
-    self->x = self->x / to_divide;
-    self->y = self->y / to_divide;
-    self->z = self->z / to_divide;
+    out[0] = -a[0];
+    out[1] = -a[1];
+    out[2] = -a[2];
 }
 
-void cge_vector3_negate(cge_vector3* self)
+float cge_vector3_magnitude_squared(float* a)
 {
-    self->x = -self->x;
-    self->y = -self->y;
-    self->z = -self->z;
+    return (a[0] * a[0]) + (a[1] * a[1]) + (a[2] * a[2]);
 }
 
-float cge_vector3_magnitude_squared(cge_vector3* self)
+float cge_vector3_magnitude(float* a)
 {
-    float sum_of_squares = (self->x * self->x) + (self->y * self->y) + (self->z * self->z);
+    float square_sum = cge_vector3_magnitude_squared(a);
 
-    return sum_of_squares;
+    return sqrt(square_sum);
 }
 
-float cge_vector3_magnitude(cge_vector3* self)
+void cge_vector3_normalize(float* a, float* out)
 {
-    float sum_of_squares = cge_vector3_magnitude_squared(self);
+    float magnitude = cge_vector3_magnitude(a);
 
-    return sqrtf(sum_of_squares);
-}
-
-void cge_vector3_normalize(cge_vector3* self)
-{
-    float magnitude = cge_vector3_magnitude(self);
-
-    if(magnitude == 0.0f)
-    {
-        self->x = 0.f;
-        self->y = 0.f;
-        self->x = 0.f;
+    if(magnitude > 1e-6f) {
+        out[0] = a[0] / magnitude;
+        out[1] = a[1] / magnitude;
+        out[2] = a[2] / magnitude;
     }
     else
     {
-        self->x = self->x / magnitude;
-        self->y = self->y / magnitude;
-        self->z = self->z / magnitude;
+        out[0] = 0;
+        out[1] = 0;
+        out[2] = 0;
     }
 }
 
-
-float cge_vector3_distance(cge_vector3* self, cge_vector3* b)
+float cge_vector3_distance(float* a, float* b)
 {
-    cge_vector3* distance = cge_vector3_create(self->x - b->x, self->y - b->y, self->z - b->z);
-    float magnitude = cge_vector3_magnitude(distance);
-    free(distance);
+    float subtract[3] = {
+        a[0] - b[0],
+        a[1] - b[1],
+        a[2] - b[2]
+    };
 
-    return magnitude;
+    return cge_vector3_magnitude(subtract);
 }
 
-void cge_vector3_lerp(cge_vector3* self, cge_vector3* b, float t)
+float cge_vector3_dot(float* a, float* b)
 {
-    self->x = self->x + (b->x - self->x) * t;
-    self->y = self->y + (b->x - self->y) * t;
-    self->z = self->z + (b->z - self->z) * t;
+    return a[0]*b[0] + a[1]*b[1] + a[2]*b[2];
 }
 
-void cge_vector3_clamp(cge_vector3* self, cge_vector3* minimum, cge_vector3* maximum)
+void  cge_vector3_lerp(float* a, float* b, float t, float* out)
 {
-     if (self->x < minimum->x) self->x = minimum->x;
-    if (self->x > maximum->x) self->x = maximum->x;
-
-    if (self->y < minimum->y) self->y = minimum->y;
-    if (self->y > maximum->y) self->y = maximum->y;
-
-    if (self->z < minimum->z) self->z = minimum->z;
-    if (self->z > maximum->z) self->z = maximum->z;
+    out[0] = a[0] + (a[0] - b[0]) * t;
+    out[1] = a[1] + (a[1] - b[1]) * t;
+    out[2] = a[2] + (a[2] - b[2]) * t;
 }
 
-void cge_vector3_reflect(cge_vector3* self, cge_vector3* normal) {
-    float dot = self->x * normal->x + self->y * normal->y + self->z * normal->z;
+void  cge_vector3_clamp(float* a, float* minimum, float* maximum, float* out)
+{
+    if(a[0] < minimum[0])
+    {
+        out[0] = minimum[0];
+    }
+    else if(a[0] > maximum[0])
+    {
+        out[0] = maximum[0];
+    }
+    else
+    {
+        out[0] = a[0];
+    }
 
-    self->x = self->x - 2.0f * dot * normal->x;
-    self->y = self->y - 2.0f * dot * normal->y;
-    self->z = self->z - 2.0f * dot * normal->z;
+    if(a[1] < minimum[1])
+    {
+        out[1] = minimum[1];
+    }
+    else if(a[1] > maximum[1])
+    {
+        out[1] = maximum[1];
+    }
+    else
+    {
+        out[1] = a[1];
+    }
+
+    if(a[2] < minimum[2])
+    {
+        out[2] = minimum[2];
+    }
+    else if(a[2] > maximum[2])
+    {
+        out[2] = maximum[2];
+    }
+    else
+    {
+        out[2] = a[2];
+    }
 }
 
-void cge_vector3_project(cge_vector3* self, cge_vector3* b) {
-    float dot_ab = self->x * b->x + self->y * b->y + self->z * b->z;
+void cge_vector3_reflect(float* a, float* b, float* out)
+{
+    float dot = cge_vector3_dot(a, b);
+    float scale = 2.0f * dot;
 
-    float dot_bb = b->x * b->x + b->y * b->y + b->z * b->z;
-
-    float scalar = dot_ab / dot_bb;
-
-    self->x = scalar * b->x;
-    self->y = scalar * b->y;
-    self->z = scalar * b->z;
+    out[0] = a[0] - scale * b[0];
+    out[1] = a[1] - scale * b[1];
+    out[2] = a[2] - scale * b[2];
 }
 
-void cge_vector3_reject(cge_vector3* self, cge_vector3* b) {
-    float dot_ab = self->x * b->x + self->y * b->y + self->z * b->z;
-    float dot_bb = b->x * b->x + b->y * b->y + b->z * b->z;
-    float scalar = dot_ab / dot_bb;
+void  cge_vector3_project(float*  a, float* b, float* out)
+{
+    float a_dot = cge_vector3_dot(a, b);
+    float b_magnitude_squared = cge_vector3_magnitude_squared(b);
 
-    float proj_x = scalar * b->x;
-    float proj_y = scalar * b->y;
-    float proj_z = scalar * b->z;
-
-    self->x = self->x - proj_x;
-    self->y = self->y - proj_y;
-    self->z = self->z - proj_z;
+    if (b_magnitude_squared != 0.0f) {
+        float scale = a_dot / b_magnitude_squared;
+        out[0] = scale * b[0];
+        out[1] = scale * b[1];
+        out[2] = scale * b[2];
+    }
+    else
+    {
+        out[0] = out[1] = out[2] = 0.0f;
+    }
 }
+}
+
+void  cge_vector3_reject(float*  self, float*  b, float* out);
+
+float*  cge_vector3_rotate_by_quaternion(float* a, float* quaternion, float* out);
+float*  cge_vector3_hadamard(float* a, float* b, float* out);
