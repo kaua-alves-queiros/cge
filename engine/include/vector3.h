eedd43aff4a2df2fdcade51b1493a78fa5f022ef
Kauã Q. <89699012+kaua-alves-queiros@users.noreply.github.com>
2025-08-09 14:09:13 +0000

    re design vector3

--- engine/include/vector3.h
+++ engine/include/vector3.h
@@ -3,41 +3,30 @@
 #include <stdlib.h>
 #include "./vector4.h"
 
-typedef struct cge_vector3 cge_vector3;
+void cge_vector3_add(float* a, float* b, float* out);
+void cge_vector3_subtract(float* a, float* b, float* out);
+void cge_vector3_multiply(float* a, float* b, float* out);
+void cge_vector3_divide(float* a, float* b, float* out);
 
-struct cge_vector3
-{
-    float x;
-    float y;
-    float z;
-};
+void cge_vector3_add_scalar(float* a, float b, float* out);
+void cge_vector3_subtract_scalar(float* a, float b, float* out);
+void cge_vector3_multiply_scalar(float* a, float b, float* out);
+void cge_vector3_divide_scalar(float* a, float b, float* out);
 
+void cge_vector3_negate(float* a, float* out);
 
-cge_vector3* cge_vector3_create(float x, float y, float z);
+float cge_vector3_magnitude_squared(float* a);
+float cge_vector3_magnitude(float* a);
+void cge_vector3_normalize(float* a, float* out);
+float cge_vector3_distance(float* a, float* b);
+float cge_vector3_dot(float* a, float* b);
+void  cge_vector3_lerp(float* a, float* b, float t, float* out);
+void  cge_vector3_clamp(float* a, float* minimum, float* maximum, float* out);
+void  cge_vector3_reflect(float* a, float* b, float* out);
+void  cge_vector3_project(float*  a, float* b, float* out);
+void  cge_vector3_reject(float*  self, float*  b, float* out);
 
-void cge_vector3_add(cge_vector3* self, cge_vector3* to_add);
-void cge_vector3_subtract(cge_vector3* self, cge_vector3* to_subtract);
-void cge_vector3_multiply(cge_vector3* self, cge_vector3* to_multiply);
-void cge_vector3_divide(cge_vector3* self, cge_vector3* to_divide);
+float*  cge_vector3_rotate_by_quaternion(float* a, float* quaternion, float* out);
+float*  cge_vector3_hadamard(float* a, float* b, float* out);
 
-void cge_vector3_add_scalar(cge_vector3* self, float to_add);
-void cge_vector3_subtract_scalar(cge_vector3* self, float to_subtract);
-void cge_vector3_multiply_scalar(cge_vector3* self, float to_multiply);
-void cge_vector3_divide_scalar(cge_vector3* self, float to_divide);
-
-void cge_vector3_negate(cge_vector3* self);
-
-float cge_vector3_magnitude_squared(cge_vector3* self);
-float cge_vector3_magnitude(cge_vector3* self);
-void cge_vector3_normalize(cge_vector3* self);
-float cge_vector3_distance(cge_vector3* self, cge_vector3* b);
-
-void cge_vector3_lerp(cge_vector3* self, cge_vector3* b, float t);
-void cge_vector3_clamp(cge_vector3* self, cge_vector3* minimum, cge_vector3* maximum);
-void cge_vector3_reflect(cge_vector3* self, cge_vector3* normal);
-void cge_vector3_project(cge_vector3* self, cge_vector3* b);
-void cge_vector3_reject(cge_vector3* self, cge_vector3* b);
-
-void cge_vector3_rotate_by_quaternion(cge_vector3* self, cge_vector4* quaternion);
-void cge_vector3_hadamard(cge_vector3* self, cge_vector3* b);
 #endif
